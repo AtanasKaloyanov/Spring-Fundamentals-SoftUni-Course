0ed5da89ecd83e1c3fae65f56ee834f78b610b7e
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2022-12-27 23:23:24 +0200

    Spring Introduction (MVC) project

---
+++ T02 Spring Introduction (MVC)/SpringMVC/src/main/java/bg/softuni/SpringMVC/web/UserController.java
@@ -0,0 +1,23 @@
+package bg.softuni.SpringMVC.web;
+
+import bg.softuni.SpringMVC.model.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String newUser() {
+        return "newuser";
+    }
+
+    @PostMapping
+    public String createUser(UserDTO userDTO) {
+        System.out.println("Creating new user..." + userDTO);
+        return "usercreated";
+    }
+}
